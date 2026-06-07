386e8b3801f8199b238905bb8d3a31d30cb265b9
Anna <anna@gmail.com>
2026-06-07 15:16:17 +0300

    Extend item model for clothing shop

--- src/main/java/vibetower/model/Item.java
+++ src/main/java/vibetower/model/Item.java
@@ -1,22 +1,70 @@
 package vibetower.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Item implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private String name;
+    private String category;
     private int price;
+    private String currency;
+    private int minLevel;
 
     public Item(String name, int price) {
+        this(name, "Інтер'єр", price, "silver", 1);
+    }
+
+    public Item(String name, String category, int price, String currency, int minLevel) {
         this.name = name;
+        this.category = category;
         this.price = price;
+        this.currency = currency;
+        this.minLevel = minLevel;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
     public int getPrice() {
         return price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public int getMinLevel() {
+        return minLevel;
+    }
+
+    public String getPriceText() {
+        if (currency.equals("gold")) {
+            return price + " золота";
+        }
+
+        return price + " срібла";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Item other)) {
+            return false;
+        }
+
+        return Objects.equals(name, other.name)
+                && Objects.equals(category, other.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, category);
     }
 }
